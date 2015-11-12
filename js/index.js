8fd973c9f8bb531359379d577101d11651a1f888
张根茂 <zgm4454@gmail.com>
2015-11-13 00:10:14 +0800

    坐标箭头
    右侧坐标箭头添加

--- js/index.js
+++ js/index.js
@@ -17,14 +17,14 @@
 
 	//X轴
 	var scale1_x = d3.scale.linear().domain([0, 24]).range([0, g_width])
-	var scale2_x = d3.scale.linear().domain([0, 0]).range([0, g_width])
+	var scale2_x = d3.scale.linear().domain([0, 0]).range([-20, g_width])
 	//左侧刻度
 	var scale1_y = d3.scale.linear().domain([0, 5000]).range([250, 0])
 	var scale2_y = d3.scale.linear().domain([0, 6000]).range([0, 300])
 
 	//右侧刻度
 	var scale_y_r1 = d3.scale.linear().domain([0, 100]).range([250, 0])
-	var scale_y_r2 = d3.scale.linear().domain([40, 100]).range([0, 250])
+	var scale_y_r2 = d3.scale.linear().domain([40, 100]).range([0, 200])
 
 	//可以的左右轴
 	var move_l = d3.scale.linear().domain([0, 0]).range([500, 0])
@@ -41,33 +41,74 @@
 	//添加XY轴标尺
 	var x_axis = d3.svg.axis().scale(scale1_x).tickValues([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]);					//时间轴
 	var x2_axis = d3.svg.axis().scale(scale2_x).outerTickSize([0]);																				//0刻度线
-	var y_axis = d3.svg.axis().scale(scale1_y).tickValues([0,1000,2000,3000,4000,5000]).orient("left");											//左侧上部刻度
-	var y2_axis = d3.svg.axis().scale(scale2_y).tickValues([0,1000,2000,3000,4000,5000,6000]).orient("left");									//左侧下部刻度
-	var y_axis_r1 = d3.svg.axis().scale(scale_y_r1).tickValues([0,20,40,60,80,100]).orient("right");											//右测上部刻度
-	var y_axis_r2 = d3.svg.axis().scale(scale_y_r2).tickValues([40,55,70,85,100]).orient("right");												//右测下部刻度
+	var y_axis = d3.svg.axis().scale(scale1_y).tickSize(0,0).tickPadding(30).tickValues([0,1000,2000,3000,4000,5000]).orient("left");			//左侧上部刻度
+	var y2_axis = d3.svg.axis().scale(scale2_y).tickSize(0,0).tickPadding(30).tickValues([0,1000,2000,3000,4000,5000,6000]).orient("left");									//左侧下部刻度
+	var y_axis_r1 = d3.svg.axis().scale(scale_y_r1).tickSize(0,0).tickValues([0,20,40,60,80,100]).orient("right");								//右测上部刻度
+	var y_axis_r2 = d3.svg.axis().scale(scale_y_r2).tickSize(0,0).tickValues([55,70,85,100]).orient("right");									//右测下部刻度
 
 	//可移动左右轴
-	var moveL_axis = d3.svg.axis().scale(move_l).orient("left").outerTickSize([0]);																				//左移动轴
-	var moveR_axis = d3.svg.axis().scale(move_r).orient("left").outerTickSize([0]);																				//右移动轴
-
+	var moveL_axis = d3.svg.axis().scale(move_l).orient("left").outerTickSize([0]);																//左移动轴
+	var moveR_axis = d3.svg.axis().scale(move_r).orient("left").outerTickSize([0]);																//右移动轴
 
 	g.append("g").call(x_axis).attr("transform", "translate(0," + g_height +")").append("text").text("H").attr("transform", "translate(1010,30)")//时间轴
 	g.append("g").call(y_axis)
 	g.append("g").call(y_axis_r1).attr("transform", "translate(1030,0)")
 
-	gBatt.append("g").call(x2_axis)		//0刻度线
+	gBatt.append("g").call(x2_axis).append("text").text("40").attr("transform", "translate(1038,16)")		//0刻度线
 	gBatt.append("g").call(y2_axis)
 	gBatt.append("g").call(y_axis_r2).attr("transform", "translate(1030,0)")
 
-	// ////绘制右侧移动轴圆和直线
-	// g.append("g").call(moveL_axis).attr("transform", "translate(200,0)").append("circle").attr("cx", 0).attr("cy", 0).attr("r",5)
-	// 	.on("mouseover", function() {
-	// 		d3.select(this).transition().duration(200).attr('r', 7);
-	// 	})
-	// 	.on("mouseout", function() {
-	//  		d3.select(this).transition().duration(200).attr('r', 5);
-	// 	})
+//会绘制右侧箭头
+var defs = svg.append("defs");
 
+var arrowMarker = defs.append("marker")
+						.attr("id","arrow")
+						.attr("markerUnits","strokeWidth")
+					    .attr("markerWidth","12")
+                        .attr("markerHeight","12")
+                        .attr("viewBox","0 0 12 12")
+                        .attr("refX","6")
+                        .attr("refY","6")
+                        .attr("orient","auto");
+
+var arrow_path = "M2,2 L10,6 L2,10 L6,6 L2,2";
+
+arrowMarker.append("path")
+			.attr("d",arrow_path)
+			.attr("fill","#000");
+
+var line = svg.append("line")
+			 .attr("x1",0)
+			 .attr("y1",0)
+			 .attr("x2",200)
+			 .attr("y2",50)
+			 .attr("stroke","red")
+			 .attr("stroke-width",2)
+			 .attr("marker-end","url(#arrow)");
+
+
+
+
+
+
+
+
+	//曲线图背景直线
+	var bglineY = 100;
+	for (var i = 0; i < 11; i++) {
+		svg.append("line")
+			.attr("class", "bg-line")
+			.attr("x1", 100)
+			.attr("y1", bglineY)
+			.attr("x2", 1100)
+			.attr("y2", bglineY)
+			.attr("stroke","rgb(230,230,230)")
+			.attr("stroke-width",1);
+
+			bglineY+=50;
+	};
+
+//绘制左右可移动轴
 	var LeftPointX = 400;	//左侧圆点X Y坐标
 	var LeftPointY = 650;
 	var RightPointX = 800;	//右侧圆点X Y坐标
@@ -82,7 +123,7 @@
 			if( d3.event.x >=100 && d3.event.x <= 1100)
 			{
 				var X = document.getElementById("moveL").getAttribute("cx");			//圆点和直线圆点X坐标
-				var popoverL = X - 230;
+				var popoverL = parseInt(X)- 286;
 
 				d3.select(this)
 				  .attr("cx", d.cx = d3.event.x )
@@ -115,7 +156,7 @@
 		.attr("cx",LeftPointX)
 		.attr("cy",100)
 		.attr("r",function(d){ return d.r; })	//圆点半径
-		.attr("fill","black")
+		.attr("fill","#B3B3B3")
 		.on("mouseover", function() {
 				d3.select(this).transition().duration(200).attr('r', 7);
 			})
@@ -131,8 +172,9 @@
 		.attr("y1", 100)
 		.attr("x2", LeftPointX)
 		.attr("y2", LeftPointY)
-		.attr("stroke","red")
+		.attr("stroke","#B3B3B3")
 		.attr("stroke-width",1);
+
 
 
 
@@ -145,16 +187,17 @@
 			if( d3.event.x >=100 && d3.event.x <= 1100)
 			{
 				var X = document.getElementById("moveR").getAttribute("cx");			//圆点和直线圆点X坐标
+				var xR = parseInt(X)+ 10;
 
 				d3.select(this)
 				  .attr("cx", d.cx = d3.event.x )
 				  .attr("cy", d.cy = 100 );
 				d3.select("#lineR")
-				.attr("x1", function () {var x1 = X; return x1; })
-				.attr("x2", function () {var x2 = X; return x2; });
+				.attr("x1", function () {var x = X; return x; })
+				.attr("x2", function () {var x = X; return x; });
 
 				d3.select("#popoverRight")
-				.attr("style", "display: block; top: 100px;" + "left:"+ X +"px; ")
+				.attr("style", "top: 100px;" + "left:"+ xR +"px; display: block; ")
 
 			}
 		}else if( d.cx < 100){
@@ -178,7 +221,7 @@
 		.attr("cx",RightPointX)
 		.attr("cy",100)
 		.attr("r",function(d){ return d.r; })	//圆点半径
-		.attr("fill","black")
+		.attr("fill","#B3B3B3")
 		.on("mouseover", function() {
 				d3.select(this).transition().duration(200).attr('r', 7);
 			})
@@ -194,25 +237,8 @@
 		.attr("y1", 100)
 		.attr("x2", RightPointX)
 		.attr("y2", RightPointY)
-		.attr("stroke","red")
+		.attr("stroke","#B3B3B3")
 		.attr("stroke-width",1);
-
-	//右轴数据显示框
-	// svg.append("div")
-	// 	.attr("id", "popoverRight")
-	// 	.attr("class", "popover fade right in")
-	// 	.attr("role", "tooltip")
-	// 	.attr("style", "display: block; width: 300px; height: 300px; top :100px; left: 800px; background: red;");
-
-	// svg.select("#popoverRight")
-	// 	.append("div")
-	// 	.attr("class","arrow")
-	// 	.attr("style","top: 50%;")
-	// 	.append("div")
-	// 	.attr("class","popover-content")
-	// 	.append("ul")
-	// 	.append("li")
-	// 	.text("asdfdsafdsasd");
 
 
 
